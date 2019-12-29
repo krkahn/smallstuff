a495614d684e48bac439a1cd21d0f1a52dfbc120
Kahn <kahn@kahnbox.kahn.net>
2019-12-28 21:51:07 -0500

    changed to include process exporter

--- src/Main.go
+++ src/Main.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
 )
 
+const directory string = "/proc/"
+const stat string = "/stat"
+
+func isNumeric(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
+
 func main() {
-	fmt.Print("Go runs on ")
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux.")
-	default:
-		// freebsd, openbsd,
-		// plan9, windows...
-		fmt.Printf("%s.\n", os)
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if isNumeric(f.Name()) {
+			//if contains any non numberic characters, break
+			var FullPath = strings.Join([]string{directory, f.Name(), stat}, "")
+			fmt.Println(FullPath, "\t")
+			content, err := ioutil.ReadFile(FullPath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("File contents:\t%s", content)
+		} else {
+			continue
+		}
+		//fmt.Println(directory, f.Name(), stat)
+		//statsdir, err := ioutil.ReadDir()
 	}
 }
